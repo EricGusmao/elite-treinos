675fe2d516d9fec31bc17b290d802a3ceeeabfe6
Eric Gusmao <ericdegusmaopino@gmail.com>
2026-03-05 07:47:11 -0300

    format files

--- frontend/components/link.tsx
+++ frontend/components/link.tsx
@@ -1,12 +1,15 @@
-import * as Headless from '@headlessui/react'
-import React, { forwardRef } from 'react'
-import { Link as RouterLink, type LinkProps } from "react-router"
+import * as Headless from "@headlessui/react";
+import type React from "react";
+import { forwardRef } from "react";
+import { Link as RouterLink, type LinkProps } from "react-router";
 
 export const Link = forwardRef(function Link(
-    { href, ...props }: { href: LinkProps['to'] } & Omit<LinkProps, 'to'>,
-    ref: React.ForwardedRef<HTMLAnchorElement>) {
-    return (
-        <Headless.DataInteractive>
-            <RouterLink {...props} to={href} ref={ref} />
-        </Headless.DataInteractive>
-    )})
+	{ href, ...props }: { href: LinkProps["to"] } & Omit<LinkProps, "to">,
+	ref: React.ForwardedRef<HTMLAnchorElement>,
+) {
+	return (
+		<Headless.DataInteractive>
+			<RouterLink {...props} to={href} ref={ref} />
+		</Headless.DataInteractive>
+	);
+});
