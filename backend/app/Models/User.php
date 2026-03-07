ca9c2de2443844144435d912e72403f87c780010
Eric Gusmao <ericdegusmaopino@gmail.com>
2026-03-07 14:03:20 -0300

    add laravel rector

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,8 +12,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 final class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
